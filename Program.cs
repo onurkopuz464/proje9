52b35178e71cc330b73658afb6387f6293589ea2
onurkopuz464 <onurkopuz464@gmail.com>
2022-10-22 18:10:41 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace c
+{
+    class Program
+    {
+     public static void staircase(int sayi)
+    {
+        int[] sayilar = new int[sayi];
+
+        for (int i = 0; i < sayi; i++)
+        {
+            sayilar[i] = i;
+        }
+
+
+
+        foreach (var item in sayilar)
+        {
+            if(item%2==0)
+            {
+            Console.WriteLine(item);
+            }
+        }
+        
+    }
+
+    static void Main(string[] args)
+    {
+        int n= Convert.ToInt32(Console.ReadLine().Trim());
+        
+        staircase(n);
+         
+        
+
+    }
+        
+        
+      
+    }
+    
+}
